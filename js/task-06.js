46316f838868807a379fc8d1ff368e1d1cb1c832
lwisniewski96 <113308030+lwisniewski96@users.noreply.github.com>
2023-03-07 22:08:18 +0100

    final

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,14 @@
-const validationInput = document.querySelector("#validation-input");
+const validationInput = document.querySelector(`#validation-input`);
 
 validationInput.addEventListener(`blur`, (event) => {
-  const dataLength = validationInput.getAttribute("data-length");
+  const length = validationInput.getAttribute(`data-length`);
   if (validationInput.value.length === parseInt(length)) {
     validationInput.classList.remove(`invalid`);
     validationInput.classList.add(`valid`);
   } else {
     validationInput.classList.remove(`valid`);
-    validationInput.classList.add("invalid");
+    validationInput.classList.add(`invalid`);
   }
+
+  console.log(validationInput);
 });
